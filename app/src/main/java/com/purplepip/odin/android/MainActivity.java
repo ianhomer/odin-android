ed940adda4572a6df73b2d2945cdcc3df47f3b86
Ian Homer <ian.homer@gmail.com>
2017-04-17 18:23:42 +0100

    Output MIDI device to screen

--- app/src/main/java/com/purplepip/odin/android/MainActivity.java
+++ app/src/main/java/com/purplepip/odin/android/MainActivity.java
@@ -1,7 +1,12 @@
 package com.purplepip.odin.android;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.midi.MidiDeviceInfo;
+import android.media.midi.MidiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView t = (TextView) findViewById(R.id.text);
+        t.append("\nTesting");
+
+        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_MIDI)) {
+            t.append("\nMIDI enabled");
+            MidiManager m = (MidiManager) getSystemService(Context.MIDI_SERVICE);
+            MidiDeviceInfo[] infos = m.getDevices();
+            t.append("\nMIDI device count : " + infos.length);
+
+            for (MidiDeviceInfo info : infos) {
+                t.append("\n" + info.toString());
+            }
+        } else {
+            t.append("\nNo MIDI");
+        }
+
+        t.append("\n ... DONE");
+
     }
 }
